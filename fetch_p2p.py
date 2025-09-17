9c32a70272f94b2fb4bb16a1e8b05ed60eb27ab4
andresBD07 <andresbolivar933@gmail.com>
2025-09-17 17:16:55 -0400

    Update fetch_p2p.py

--- fetch_p2p.py
+++ fetch_p2p.py
@@ -5,7 +5,7 @@ import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Configuración
+# Configuración Binance
 URL = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
 HEADERS = { "Content-Type": "application/json" }
 
